957b3d3dbcf699047a0482a531d598869a2238f4
Luis Gonzalez <luis.ovidio.gonzalez@correounivalle.edu.co>
2015-11-16 15:05:42 -0500

    first commit

---
+++ modules.js
@@ -0,0 +1,5 @@
+var app = angular.module('servicios', []);
+	app.controller('serviciosCtrl', function($scope) {
+    
+	$scope.mantenimiento = {nombre:'mantenimiento', costo:'50000', tipoMoto:'Biz C100'};
+});
